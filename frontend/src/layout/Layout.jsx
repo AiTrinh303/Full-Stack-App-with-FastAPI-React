0cecbbf3e2fe5bb993c224d366e49556e0d9654d
AiTrinh303 <jojomapmip@gmail.com>
2026-05-04 10:34:13 +0200

    fix layout

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
-import {SignedIn, SignedOut, UserButton} from '@clerk/clerk-react'
-import {Outlet, Link, Navigate} from 'react-router-dom'
+import { SignedIn, SignedOut, UserButton, RedirectToSignIn } from '@clerk/clerk-react'
+import {Outlet, Link} from 'react-router-dom'
 
 export function Layout() {
     return (
-        <div>
+        <>
             <header className="flex justify-between items-center p-4">
-                <div className="flex gap-4">
-                    <Link to="/">Home</Link>
-                    <Link to="/history">History</Link>
-                </div>
+                <nav className="flex gap-4 items-center">
+                    <SignedIn>
+                        <Link to="/">Home</Link>
+                        <Link to="/history">History</Link>
+                    </SignedIn>
 
-                <div className="flex gap-4 items-center">
                     <SignedOut>
                         <Link to="/sign-in">Sign In</Link>
                         <Link to="/sign-up">Sign Up</Link>
                     </SignedOut>
+                </nav>
 
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                </div>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </header>
-            <main>
-                <Outlet />
+            <main className="p-4">
+                <SignedOut>
+                    <RedirectToSignIn />
+                </SignedOut>
+                <SignedIn>
+                    <Outlet />
+                </SignedIn>
             </main>
-        </div>
+        </>
     )
 }
