ced93264a2feed066ceb7b4e5a5cc47db719cd2e
AiTrinh303 <jojomapmip@gmail.com>
2026-05-04 10:27:52 +0200

    setup tailwind and layout

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import {SignedIn, SignedOut, UserButton} from '@clerk/clerk-react'
+import {Outlet, Link, Navigate} from 'react-router-dom'
 
 export function Layout() {
-    return <>Layout</>
+    return (
+        <div>
+            <header className="flex justify-between items-center p-4">
+                <div className="flex gap-4">
+                    <Link to="/">Home</Link>
+                    <Link to="/history">History</Link>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                    <SignedOut>
+                        <Link to="/sign-in">Sign In</Link>
+                        <Link to="/sign-up">Sign Up</Link>
+                    </SignedOut>
+
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
 }
