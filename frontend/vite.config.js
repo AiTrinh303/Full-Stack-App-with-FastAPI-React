d3e0bf522710536f5d399f62891754b703c6df64
AiTrinh303 <jojomapmip@gmail.com>
2026-05-12 09:39:02 +0200

    update vite config and add backend requirements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  // console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+    ...(env.VITE_DEBUG === "true" && {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+    )}
+  }
 })
